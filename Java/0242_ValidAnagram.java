3b0ba02400c4cd16cab6314727a492a9feb1a9e4
Manav Sood <manavsood2006@gmail.com>
2026-08-07 08:48:55 +0530

    Add 0242 Valid Anagram solution

---
+++ Java/0242_ValidAnagram.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int n=s.length();
+        int m=t.length();
+        if(n!=m){
+            return false;
+        }
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(char c:s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        for(char c:t.toCharArray()){
+            if(!map.containsKey(c) || map.get(c)==0){
+               return false;
+            }
+             map.put(c,map.get(c)-1);
+        }
+        return true;
+    }
+}
