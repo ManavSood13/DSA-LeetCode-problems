b60d204c56d635ed3cdf4e4063d94aef38fb05ee
Manav Sood <manavsood2006@gmail.com>
2026-08-08 18:49:24 +0530

    problem 13

---
+++ Java/13_RomanToInteger.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public int romanToInt(String s) {
+        HashMap<Character,Integer> roman=new HashMap<>();
+        roman.put('I',1);
+        roman.put('V',5);
+        roman.put('X',10);
+        roman.put('L',50);
+        roman.put('C',100);
+        roman.put('D',500);
+        roman.put('M',1000);
+        int n=s.length();
+        int result=0;
+        for(int i=0;i<n;i++){
+            if(i+1<n && roman.get(s.charAt(i)) < roman.get(s.charAt(i+1))){
+                result-=roman.get(s.charAt(i));
+            }
+            else{
+                result+=roman.get(s.charAt(i));
+            }
+        }
+        return result;
+    }
+}
