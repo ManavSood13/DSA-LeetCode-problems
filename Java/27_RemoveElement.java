7b2dc04e51e9ca0806064cd76a68da4655636f47
Manav Sood <manavsood2006@gmail.com>
2026-08-10 10:07:50 +0530

    problem 27

---
+++ Java/27_RemoveElement.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int left=0;
+        for(int right=0;right<nums.length;right++){
+            if(nums[right]!=val){
+                int temp=nums[left];
+                nums[left]=nums[right];
+                nums[right]=temp;
+                left++;
+            }
+        }
+        return left;
+    }
+}
--- Java/ListNode.java
+++ Java/ListNode.java
@@ -1,4 +1,4 @@
-
+/**
  * Definition for singly-linked list.
  * public class ListNode {
  *     int val;
@@ -8,8 +8,16 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-import java.util.*;
+
+
 class Solution {
+    public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
        
         if(list1 == null && list2 == null){
