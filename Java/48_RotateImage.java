a305f13a5b923a71c507b49a43c496d33c650127
Manav Sood <manavsood2006@gmail.com>
2026-08-11 15:10:44 +0530

    problem 48

---
+++ Java/48_RotateImage.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n=matrix.length;
+        // [1,4,7]step 1 transpose the matrix
+        // [2,5,8]
+        // [3,6,9] 
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        //step 2 rotate each row
+        for(int i=0;i<n;i++){
+            int left=0;
+            int right=n-1;
+            while(left<right){
+                int temp=matrix[i][left];
+                matrix[i][left]=matrix[i][right];
+                matrix[i][right]=temp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
