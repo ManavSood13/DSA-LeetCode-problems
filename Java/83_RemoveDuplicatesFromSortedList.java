7169f2c0d9326280b9240b11e53168b4bb4c8bf7
Manav Sood <manavsood2006@gmail.com>
2026-08-13 09:44:43 +0530

    problem 83 error fixed

--- Java/83_RemoveDuplicatesFromSortedList.java
+++ Java/83_RemoveDuplicatesFromSortedList.java
@@ -9,6 +9,13 @@
  * }
  */
 class Solution {
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
     public ListNode deleteDuplicates(ListNode head) {
         if(head==null){
             return head;
