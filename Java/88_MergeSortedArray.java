7daa0f43e54053405a4dcd72f72daeec2182f230
Manav Sood <manavsood2006@gmail.com>
2026-08-14 09:41:39 +0530

    problem 88

---
+++ Java/88_MergeSortedArray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int A=m-1;
+        int B=n-1;
+        int C=m+n-1;
+
+        while(B>=0){
+            if(A>=0 && nums1[A]>nums2[B]){
+                nums1[C--]=nums1[A--];
+            }
+            else{
+                nums1[C--]=nums2[B--];
+            }
+        }
+    }
+}
