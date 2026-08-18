5f8b6f46e6bd820c0494994810d4c45b7677ed19
Manav Sood <manavsood2006@gmail.com>
2026-08-18 10:31:37 +0530

    problem 155

---
+++ Java/155_MinStack.java
@@ -0,0 +1,57 @@
+class MinStack {
+    Stack<Long> st;
+    long min;
+    public MinStack() {
+        st=new Stack<>();
+    }
+    
+    public void push(int val) {
+        if(st.isEmpty()){
+            st.push((long) val);
+            min =val;
+        }
+        else{
+            if(val<min){
+                st.push((long) 2*val-min);
+                min=val;
+            }
+            else{
+                st.push((long) val);
+            }
+        }
+    }
+    
+    public void pop() {
+        if(st.peek()<min){
+            min=2*min-st.peek();
+            st.pop();
+        }
+        else{
+            st.pop();
+        }
+    }
+    
+    public int top() {
+        long top=st.peek();
+        if(top<min){
+            return (int) min;
+        }
+        else{
+             return (int) top;
+
+        }
+    }
+    
+    public int getMin() {
+        return (int) min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
